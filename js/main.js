2d05d6b1e59bba1e98c4369b8ee91bda01659d1b
korams <korams@students.cs.ubc.ca>
2023-03-18 21:48:23 -0700

    inited project with skeleton files

---
+++ js/main.js
@@ -0,0 +1,25 @@
+/**
+ * Load data from CSV file asynchronously and render charts
+ */
+d3.csv('data/dating.csv').then(data => {
+
+    // Convert necessary columns to numerical values
+    data.forEach(d => {
+
+    });
+
+    // Global data processing
+
+    // initialize visualizations
+    const beeswarm = new BeeswarmPlot({
+        parentElement: '#beeswarm-plot',
+    }, data);
+
+    const barChart = new BarChart({
+        parentElement: '#bar-chart',
+    }, data);
+
+    const heatMap = new HeatMap({
+        parentElement: '#heat-map',
+    }, data);
+});
