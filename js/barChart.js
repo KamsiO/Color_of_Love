dcef00eba690968026e39f38237973914e679c3c
coluka <coluka@students.cs.ubc.ca>
2023-03-25 03:30:57 -0700

    Gotten some bars showing on chart

--- js/barChart.js
+++ js/barChart.js
@@ -8,9 +8,9 @@ class BarChart {
     constructor(_config, _data) {
       this.config = {
         parentElement: _config.parentElement,
-        containerWidth: _config.containerWidth || 500,
-        containerHeight: _config.containerHeight || 500,
-        margin: _config.margin || {top: 25, right: 20, bottom: 40, left: 100},
+        containerWidth: _config.containerWidth || 400,
+        containerHeight: _config.containerHeight || 250,
+        margin: _config.margin || {top: 25, right: 20, bottom: 40, left: 50},
         tooltipPadding: _config.tooltipPadding || 15
       }
       
@@ -26,20 +26,32 @@ class BarChart {
       vis.width = vis.config.containerWidth - vis.config.margin.left - vis.config.margin.right;
       vis.height = vis.config.containerHeight - vis.config.margin.top - vis.config.margin.bottom;
 
+      vis.subgroupsCategory = ["yes", "no"];
+
+
       // intialize the scale 
       vis.xScale = d3.scaleBand() // segments range into equal categories based on the num of domain
         .range([0, vis.width])
-        .paddingInner(0.15)
-        .paddingOuter(0.12);
+        .paddingInner(0.5);
+
+        vis.xSubgroupScale = d3.scaleBand() // segments range into equal categories based on the num of domain
+        .range([0, vis.width])
+        // yes = couple is interracial
+        .domain(vis.subgroupsCategory)
+        .range([0, vis.xScale.bandwidth()])
+        .padding(0.05)
 
       vis.yScale = d3.scaleLinear()
-        .range([vis.height, 0]);        
+        .range([vis.height, 0]);     
+
+      // color palette = one color per subgroup
+      vis.colorScale = d3.scaleOrdinal()
+      .domain(vis.subgroupsCategory)
+      .range(['#e41a1c','#377eb8'])
+         
 
       // intialize the axis
       vis.xAxis = d3.axisBottom(vis.xScale)
-        // .ticks(6)
-        // .tickSize(10)
-        // .tickPadding(3)
         .tickSizeOuter(0);
 
         vis.yAxis = d3.axisLeft(vis.yScale)
@@ -78,7 +90,7 @@ class BarChart {
       vis.chart.append('text')
       .attr('class', 'axis-title')
       .attr('y', 0)
-      .attr('x', vis.config.margin.left - 30)
+      .attr('x', vis.config.margin.left + 23)
       .style('text-anchor', 'end')
       .text('Frequency of Rank')
       .style('font-weight', 'bold');
@@ -89,22 +101,48 @@ class BarChart {
   
     updateVis() {
       let vis = this;
+      vis.noInterracial = "no";
+      vis.yesInterracial = "yes";
+      vis.sameRaceCoupleColor = "blue";
+      vis.interracialCoupleColor = "red";
 
+      
       // group data by relationship ranking, 
       // and count number of occurences of each ranking.
-      vis.groupedData = d3.rollup(vis.data, v => v.length, d => d.Q34);
+      vis.groupedData = d3.rollup(vis.data, v => v.length, d => d.Q34, d => d.interracial_5cat);
       console.log(vis.groupedData);
            // todo: excludes the first and the last group in the groupedData
         // to exclude people with no answer, or who refused to rank
       // vis.groupedData.remove("\"\"");
       //  vis.groupedData.remove("Refused");
       vis.maxOccurenceCount = function (groupedData){
-        let mapValues = groupedData.values();
-        let max_num = Math.max(... mapValues);
+
+        // credit for iterator help: https://stackoverflow.com/a/55660647
+        let mapValuesIterator = groupedData.values();
+        let nextMapValues = mapValuesIterator.next();
+        let max_num = 0
+        
+        while (!nextMapValues.done) {
+          let currMax = 0;
+          let innerMap = nextMapValues.value;
+
+          // turn map to array: https://stackoverflow.com/a/56795800
+          let innerMapAsArray = Array.from(innerMap, ([name, value]) => ({ name, value }));
+          
+          // console.log(innerMapAsArray);
+          // console.log(innerMapAsArray[0]);
+          // console.log(innerMapAsArray[0].name);
+
+          currMax = Math.max(innerMapAsArray[0].value, innerMapAsArray[1].value);
+          if( currMax > max_num) {
+            max_num = currMax;
+          }
+          nextMapValues = mapValuesIterator.next();
+        } 
         return max_num;
       }
 
-      console.log(vis.maxOccurenceCount(vis.groupedData));
+      // console.log(vis.maxOccurenceCount(vis.groupedData));
 
       // Specificy x- and y-accessor functions
       vis.xValue = d => d.Q34;
@@ -116,7 +154,7 @@ class BarChart {
       vis.xScale.domain(["Very Poor", "Poor", "Fair", "Good", "Excellent"]);
       vis.yScale.domain([0,vis.maxOccurenceCount(vis.groupedData)]);
 
-      console.log("got here");
+      console.log(this.yScale(23));
       vis.renderVis();
       
     }
@@ -128,34 +166,78 @@ class BarChart {
       vis.pointsPlotted = 0;
       vis.totalPointsToPlot = 0;
 
-      const bars = vis.chart.selectAll('.bar')
-          .data(vis.data, d => d.Q34)
-        .join('rect')
+      // code for bars and bar inspired from here: https://d3-graph-gallery.com/graph/barplot_grouped_basicWide.html
+      const bars = vis.chart.selectAll('.bars')
+          .data(vis.groupedData)
+          .join("g")
           .filter(d => {
+            console.log(d);
+            console.log(d[0]);
             //removes those that didn't share their race 
             // data with the rank section blank and those that refused to rank
-            if(d.interracial_5cat != "" && d.Q34 != "" && d.Q34 != "Refused") {
-              vis.pointsPlotted += 1;
+            if( d[0] != "" && d[0] != "Refused") {
+              // vis.pointsPlotted += vis.groupedData.get(d);
               return d;
             }
           })
-          .attr('class', 'bar')
-          .attr('width', d => {
-            // console.log(vis.xScale.bandwidth());
-            return vis.xScale.bandwidth();
-          })
-          .attr('height', d => {
-            return vis.yScale(vis.yValue(d)) + 20;
-          })
-          .attr('x', d =>  vis.xScale(vis.xValue(d)))
-          .attr('y', d => vis.height - vis.yScale(vis.yValue(d)));
+          .attr('class', 'bars')
+          .attr("transform", d => {
+            // console.log(d[0]);
+          return `translate( ${vis.xScale(d[0])- vis.xScale.bandwidth()/2},0)`});
 
-          console.log("got here");
+      const bar = bars.selectAll('.bar')
+          .data (d => [d[1]])
+          .join('g')
+            .selectAll('g')
+            .data(d => [d])
+              .join ('rect')
+              .attr('class', 'bar')
+              .attr('x', (d, index)=>  {
+                console.log(d);
+                console.log(d.key);
+                if (index == 0) {
+                  console.log(vis.xSubgroupScale('no'));
+                  return vis.xSubgroupScale('no');
+                } else {
+                  return vis.xSubgroupScale('yes');
+                }           
+              })
+              .attr('y', (d, index) => {
+                if (index == 0) {
+                  console.log(vis.yScale(d.get('no')));
+                  return vis.yScale(d.get('no'));
+                } else {
+                  console.log(d.get('yes'));
+                  return vis.yScale(d.get('yes'));
+                }
+              })
+              .attr('width', d => {
+                // console.log(vis.xScale.bandwidth());
+                return vis.xSubgroupScale.bandwidth();
+              })
+              .attr('height', (d, index) => {
+                if (index == 0) {
+                  console.log(d.get('no'));
+                  return vis.height -  vis.yScale(d.get('no'));
+                } else {
+                  return vis.height -  vis.yScale(d.get('yes'));
+                }
+                return vis.yScale(d);
+              })
+              .attr("fill", (d, index) => {
+                if(index == 0) {
+                  return vis.sameRaceCoupleColor;
+                } else {
+                  return vis.interracialCoupleColor;
+                }
+              });
+        
+
 
       // add text explaining how many points were used.
       document.getElementById("num-of-points-plotted").innerHTML = `${vis.pointsPlotted}/${vis.totalPointsToPlot} data points were plotted`;
 
-      bars
+      bar
         .on('mouseover', function (event,d) {
           vis.toolTipInfo(event,d);
         })
