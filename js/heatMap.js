17272d801f7d3d73ce6f6492500cb7b1c460dcfd
korams <korams@students.cs.ubc.ca>
2023-03-31 01:19:43 -0700

    small multiples: double the trouble

--- js/heatMap.js
+++ js/heatMap.js
@@ -5,19 +5,20 @@ class HeatMap {
      * @param {Object}
      * @param {Array}
      */
-    constructor(_config, _data) {
+    constructor(_config, _data, _chart_id) {
       this.config = {
         parentElement: _config.parentElement,
         containerWidth: 380,
-      containerHeight: 300,
-        margin: {top: 30, right: 20, bottom: 100, left: 130}
+        containerHeight: 300,
+        margin: _config.margin || {top: 30, right: 20, bottom: 100, left: 130}
       }
       this.data = _data;
-      this.colors = [
-        "#dddddd", "#7bb3d1", "#016eae",
-        "#dd7c8a", "#8d6c8f", "#4a4779",
-        "#cc0024", "#8a274a", "#4b264d"
-      ];
+      this.chart_id = _chart_id;
+      // this.colors = [
+      //   "#dddddd", "#7bb3d1", "#016eae",
+      //   "#dd7c8a", "#8d6c8f", "#4a4779",
+      //   "#cc0024", "#8a274a", "#4b264d"
+      // ];
       
       this.initVis();
     }
@@ -29,19 +30,24 @@ class HeatMap {
       vis.width = vis.config.containerWidth - vis.config.margin.left - vis.config.margin.right;
       vis.height = vis.config.containerHeight - vis.config.margin.top - vis.config.margin.bottom;
 
-      // filter out data points where there was no response
-      vis.data = vis.data.filter(d => {
-        let responded_sex_freq = d.w6_sex_frequency != "" && d.w6_sex_frequency != "Refused";
-        let responded_religious = d.ppp20072 != "" && d.ppp20072 != "Refused";
-        return responded_sex_freq && responded_religious;
-      });
+      // // filter out data points where there was no response
+      // vis.data = vis.data.filter(d => {
+      //   let responded_sex_freq = d.w6_sex_frequency != "" && d.w6_sex_frequency != "Refused";
+      //   let responded_religious = d.ppp20072 != "" && d.ppp20072 != "Refused";
+      //   let has_cohab_value = d.cohab_before_marriage != "";
+      //   return responded_sex_freq && responded_religious && has_cohab_value;
+      // });
 
-      vis.data.forEach(d => {
-        //d.cohabit = d.S1 == "Yes, I am Married" ? 
-        // MOCK FOR NOW:
-        d.CaseId = +d.CaseID;
-        d.cohabit = d.CaseID % 2 == 0;
-      })
+      // group by cohabitation before marriage
+      // vis.cohabit_groups = d3.groups(vis.data, d => d.cohab_before_marriage);
+      // console.log(vis.cohabit_groups);
+
+      // vis.data.forEach(d => {
+      //   //d.cohabit = d.S1 == "Yes, I am Married" ? 
+      //   // MOCK FOR NOW:
+      //   d.CaseId = +d.CaseID;
+      //   d.cohabit = d.CaseID % 2 == 0;
+      // })
 
       vis.xScale = d3.scaleBand()
           .range([0, vis.width])
@@ -51,6 +57,9 @@ class HeatMap {
           .range([vis.height, 0])
           .padding(0.02);
 
+      vis.colorScale = d3.scaleSequential()
+          .interpolator(d3.interpolateRdPu);
+
       vis.xAxis = d3.axisBottom(vis.xScale)
           .tickSize(0)
           .tickPadding(5);
@@ -59,11 +68,20 @@ class HeatMap {
           .tickSize(0)
           .tickPadding(5);
 
-      // Define size of SVG drawing area
       vis.svg = d3.select(vis.config.parentElement).append('svg')
           .attr('width', vis.config.containerWidth)
           .attr('height', vis.config.containerHeight)
-          .attr('id', 'heat-map-chart');
+          .attr('class', 'heat-map-chart')
+          .attr('id', vis.chart_id);
+
+      // // Define small multiple svgs
+      // vis.svg = d3.select(vis.config.parentElement).selectAll("smallMultiple")
+      //     .data(vis.cohabit_groups)
+      //     .enter()
+      //   .append('svg')
+      //     .attr('width', vis.config.containerWidth)
+      //     .attr('height', vis.config.containerHeight)
+      //     .attr('id', d => `heat-map-chart-${d.cohab_before_marriage}`);
 
       vis.chart = vis.svg.append('g')
           .attr('transform', `translate(${vis.config.margin.left},${vis.config.margin.top})`);
@@ -101,25 +119,32 @@ class HeatMap {
     updateVis() {
       let vis = this;
 
-      vis.grouped = d3.flatRollup(vis.data, v => ({count: v.length, cohabit: v.filter(x => x.cohabit == true).length/v.length}), d => d.ppp20072, d => d.w6_sex_frequency);
-      console.log(vis.grouped);
+      // vis.grouped = d3.rollup(vis.data, )
 
-      // Specify x-, and y- accessor functions
+      // vis.grouped = d3.flatRollup(vis.data, v => ({count: v.length, cohabit: v.filter(x => x.cohab_before_marriage == 1).length/v.length}), d => d.ppp20072, d => d.w6_sex_frequency);
+      // console.log(vis.grouped);
+
+      vis.box_groups = d3.flatRollup(vis.data, v => v.length, d => d.ppp20072, d => d.w6_sex_frequency);
+      console.log(vis.box_groups);
+
+      // Specify x-, y-, and _____ accessor functions
       vis.xValue = d => d.ppp20072;
       vis.yValue = d => d.w6_sex_frequency;
+      vis.colorValue = d => d[2];
 
       // Specify bivariate color accessor functions
-      vis.occurences = d => d[2].count;
-      vis.cohabit = d => d[2].cohabit;
-      vis.n = Math.floor(Math.sqrt(vis.colors.length));
-      vis.xColor = d3.scaleQuantize(d3.extent(vis.grouped, vis.occurences), d3.range(vis.n));
-      vis.yColor = d3.scaleQuantize(d3.extent(vis.grouped, vis.cohabit), d3.range(vis.n));
-      vis.color = d => vis.colors[vis.xColor(vis.occurences(d)) + vis.yColor(vis.cohabit(d)) * vis.n];
+      // vis.occurences = d => d[2].count;
+      // vis.cohabit = d => d[2].cohabit;
+      // vis.n = Math.floor(Math.sqrt(vis.colors.length));
+      // vis.xColor = d3.scaleQuantize(d3.extent(vis.grouped, vis.occurences), d3.range(vis.n));
+      // vis.yColor = d3.scaleQuantize(d3.extent(vis.grouped, vis.cohabit), d3.range(vis.n));
+      // vis.color = d => vis.colors[vis.xColor(vis.occurences(d)) + vis.yColor(vis.cohabit(d)) * vis.n];
 
 
       // Set the scale input domains
-      vis.xScale.domain(vis.data.map(vis.xValue));
-      vis.yScale.domain(vis.data.map(vis.yValue));
+      vis.xScale.domain(['Never', 'Once a year or less', 'A few times a year', 'Once or twice a month', 'Once a week', 'More than once a week']);
+      vis.yScale.domain(['Once a month or less', '2 to 3 times a month', 'Once or twice a week', '3 to 6 times a week', 'Once a day or more']);
+      vis.colorScale.domain(d3.extent(vis.box_groups, vis.colorValue));
       
       vis.renderVis();
     }
@@ -135,7 +160,7 @@ class HeatMap {
       // console.log(roll);
 
       let boxes = vis.chart.selectAll('.box')
-          .data(vis.grouped)
+          .data(vis.box_groups)
         .join('rect')
           .attr('class', 'box')
           .attr('x', d => vis.xScale(d[0]))
@@ -144,7 +169,7 @@ class HeatMap {
           .attr('ry', 2)
           .attr('width', vis.xScale.bandwidth())
           .attr('height', vis.yScale.bandwidth())
-          .style('fill', d => vis.color(d));
+          .style('fill', d => vis.colorScale(vis.colorValue(d)));
           // .style("stroke-width", 4)
           // .style("stroke", "none");
 
--- js/main.js
+++ js/main.js
@@ -19,7 +19,24 @@ d3.csv('data/dating.csv').then(data => {
         parentElement: '#bar-chart',
     }, data);
 
-    const heatMap = new HeatMap({
+
+
+    // filter out data points where there was no response
+    heatmap_data = data.filter(d => {
+        let responded_sex_freq = d.w6_sex_frequency != "" && d.w6_sex_frequency != "Refused";
+        let responded_religious = d.ppp20072 != "" && d.ppp20072 != "Refused";
+        let has_cohab_value = d.cohab_before_marriage != "";
+        return responded_sex_freq && responded_religious && has_cohab_value;
+    });
+    // group by cohabitation before marriage
+    let cohabit_groups = d3.group(heatmap_data, d => d.cohab_before_marriage);
+    console.log(cohabit_groups);
+
+    const heatMapNoCohabit = new HeatMap({
         parentElement: '#heat-map',
-    }, data);
+    }, cohabit_groups.get("0"), "heatmap-no-cohabit-chart");
+    const heatMapCohabit = new HeatMap({
+        parentElement: '#heat-map',
+        margin: {top: 30, right: 150, bottom: 100, left: 0}
+    }, cohabit_groups.get("1"), "heatmap-cohabit-chart");
 });
