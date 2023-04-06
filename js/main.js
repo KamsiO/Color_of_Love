6bc2105c8a7f15bf63206a72907e781abcb7cb1a
Guramrit Bamrah <guramritb@gmail.com>
2023-04-06 01:16:59 -0700

    made new utils folder for the meeting method helper functions, made the meeting method helper functions

--- js/main.js
+++ js/main.js
@@ -1,67 +1,96 @@
-
 const MEETING_METHODS = {
-    0: "Education",
-    1: "Professional Setting",
-    2: "Social Setting",
-    3: "Internet Website",
-    4: "Online Social Networking",
-    5: "Abroad",
-    6: "Mutual Connection",
+  0: "Education",
+  1: "Professional Setting",
+  2: "Social Setting",
+  3: "Internet Website",
+  4: "Online Social Networking",
+  5: "Abroad",
+  6: "Mutual Connection",
 };
+
+// const MEETING_METHODS_FILTER_COLS = {
+//     "Education": ["hcm2017q24_school", "hcm2017q24_college"],
+//     "Professional Setting": [ "hcm2017q24_mil" === "yes" ||
+//     d.hcm2017q24_customer === "yes" ||
+//     d.hcm2017q24_vol_org === "yes" ||
+//     d.hcm2017q24_work_neighbors === "yes"]
+// }
 
 /**
  * Load data from CSV file asynchronously and render charts
  */
 let treeMap, data;
 d3.csv("data/dating.csv").then((_data) => {
-    data = _data;
-    // Convert necessary columns to numerical values
-    data.forEach((d) => {
-        //Global data processing stub
-    });
+  data = _data;
+  // Convert necessary columns to numerical values
+  data.forEach((d) => {
+    //Global data processing stub
+  });
 
-    // initialize visualizations
-    const beeswarm = new BeeswarmPlot(
-        {
-            parentElement: "#beeswarm-plot",
-        },
-        data
-    );
+  // initialize visualizations
+  const beeswarm = new BeeswarmPlot(
+    {
+      parentElement: "#beeswarm-plot",
+    },
+    data
+  );
 
-    const treeMap = new TreeMap(
-        {
-            parentElement: "#tree-map",
-        },
-        data
-    );
+  const treeMap = new TreeMap(
+    {
+      parentElement: "#tree-map",
+    },
+    data,
+    {
+      checkEducationMethod,
+      checkProfessionalSettingMethod,
+      checkSocialSettingMethod,
+      checkInternetSiteMethod,
+      checkOnlineSocialNetworkingMethod,
+      checkAbroadMethod,
+      checkMutualConnectionMethod,
+    }
+  );
 
-    const heatMap = new HeatMap({
-        parentElement: '#heat-map',
-    }, data);
+  const heatMap = new HeatMap(
+    {
+      parentElement: "#heat-map",
+    },
+    data
+  );
 
+  // https://stackoverflow.com/questions/24193593/d3-how-to-change-dataset-based-on-drop-down-box-selection
+  // event listeners for the dropdown
 
-// https://stackoverflow.com/questions/24193593/d3-how-to-change-dataset-based-on-drop-down-box-selection
-// event listeners for the dropdown
-
-// the flag ensures that we don't re-filter the data if we don't need to
-let currSelection;
-d3.selectAll("#age-group-filter-dropdown").on("change", function (e) {
+  // the flag ensures that we don't re-filter the data if we don't need to
+  let currSelection;
+  d3.selectAll("#age-group-filter-dropdown").on("change", function (e) {
     // Filter data accordingly and update vis
 
     if (currSelection !== e.target.value) {
-        currSelection = e.target.value;
-        if (currSelection == "all") {
-            treeMap.data = data;
-            heatMap.data = data;
-        } else {
-            treeMap.data = data.filter(function (d) {
-                return d.ppagecat === currSelection;
-            });
-            heatMap.data = data.filter(d => d.ppagecat === currSelection);
-        }
-        treeMap.updateVis();
-        heatMap.updateVis();
+      currSelection = e.target.value;
+      if (currSelection == "all") {
+        treeMap.data = data;
+        heatMap.data = data;
+      } else {
+        treeMap.data = data.filter(function (d) {
+          return d.ppagecat === currSelection;
+        });
+        heatMap.data = data.filter((d) => d.ppagecat === currSelection);
+      }
+      treeMap.updateVis();
+      heatMap.updateVis();
     }
-});
+  });
 
+  /**
+   * Use bar chart as filter and update scatter plot accordingly
+   */
+  // function filterData() {
+  //     if (difficultyFilter.length == 0) {
+  //       scatterplot.data = data;
+  //     } else {
+  //       scatterplot.data = data.filter(d => difficultyFilter.includes(d.difficulty));
+  //     }
+  //     scatterplot.updateVis();
+  //   }
 });
